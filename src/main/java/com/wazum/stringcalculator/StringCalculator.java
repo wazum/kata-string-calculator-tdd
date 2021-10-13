3882d2ab496643e1ce01caae7ef450cd7d5fe5fe
Wolfgang Klinger <wk@plan2.net>
2021-10-13 09:14:33 +0200

    Support different delimiters (#4)
    The first line specifies the delimiter if it starts with //

--- src/main/java/com/wazum/stringcalculator/StringCalculator.java
+++ src/main/java/com/wazum/stringcalculator/StringCalculator.java
@@ -2,21 +2,27 @@ package com.wazum.stringcalculator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 public class StringCalculator {
     public static int add(String numbers) {
-        return split(clean(numbers))
-                .stream()
+        String delimiter = ",";
+        Optional<String> firstLine = Arrays.stream(numbers.split("\\n")).findFirst();
+        if (firstLine.isPresent() && firstLine.get().startsWith("//") && !firstLine.get().substring(2).isBlank()) {
+            delimiter = firstLine.get().substring(2).trim();
+        }
+
+        return split(clean(numbers, delimiter), delimiter).stream()
                 .mapToInt(Integer::parseInt)
                 .sum();
     }
 
-    private static String clean(String numbers) {
-        return numbers.replaceAll("[^,\\n+\\d+]", "");
+    private static String clean(String numbers, String delimiter) {
+        return numbers.replaceAll(String.format("[^%s\\n+\\d+]", delimiter), "");
     }
 
-    private static ArrayList<String> split(String numbers) {
-        String[] splitNumbers = numbers.split("[,\\n]");
+    private static ArrayList<String> split(String numbers, String delimiter) {
+        String[] splitNumbers = numbers.split(String.format("[%s\\n]", delimiter));
         ArrayList<String> list = new ArrayList<>(Arrays.asList(splitNumbers));
         list.removeIf(String::isBlank);
 
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -50,4 +50,10 @@ public class StringCalculatorTest {
     {
         assertEquals(6, StringCalculator.add("1\n2,3"));
     }
+
+    @Test
+    void stringWithDelimiterLineAndOneTwoThreeIsSplitByDelimiterAndReturnsSix ()
+    {
+        assertEquals(6, StringCalculator.add("//;\n1;2;3"));
+    }
 }
